199e2437253525e984c52ef816f2738c05a262cb
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-02-10 16:57:47 +0530

    Create `walk` function to retrieve all file-paths

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> std::io::Result<()> {
+    let root = std::path::Path::new(".");
+    let files = loc::walk(root);
+    for file in files {
+        println!("{}", file.display());
+    }
+    Ok(())
 }
