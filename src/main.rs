7c0da2f771696b6e559d3d9212f2eb67cac33bc7
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-02-11 19:17:52 +0530

    Separate `run` logic out of `main`

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,25 @@ struct Args {
     directory: String,
 }
 
+/// The main entry-point of the application
 fn main() -> std::io::Result<()> {
     // Parse the command-line arguments
     let args = Args::parse();
+    // Run the main logic with the given command-line arguments
+    if let Err(e) = run(&args) {
+        eprintln!("Error: {}", e);
+        std::process::exit(1)
+    }
+    Ok(())
+}
 
+/// Run the main logic of the application
+fn run(args: &Args) -> std::io::Result<()> {
     // Perform the scanning operation
     let result = loc::scan(&args.directory)?;
 
     // Print the results
     println!("{}", result.display());
+
     Ok(())
 }
