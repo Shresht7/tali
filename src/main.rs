e88e952ab183ad44eae207cd69ff3dd6c7fd1283
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-02-13 12:58:38 +0530

    Update comments

--- src/main.rs
+++ src/main.rs
@@ -9,19 +9,21 @@ fn main() -> std::io::Result<()> {
     Ok(())
 }
 
-/// Run the main logic of the application
+/// Run the main logic of the application by scanning the provided paths and then displaying the results.
 fn run(args: &cli::Args) -> std::io::Result<()> {
+    // Scan the paths for the metrics
     let results = loc::scan(&args.paths)?;
 
+    // Create a default display configuration
     let mut display = loc::Display::default();
 
     // If all the flags are false, then do nothing and just use the defaults
-    let all_modes = vec![args.lines, args.words, args.chars, args.bytes]
+    let show_all = vec![args.lines, args.words, args.chars, args.bytes]
         .iter()
         .all(|toggle| toggle == &false);
 
     // If even a single flag was specified, then adhere to that selection and override the defaults
-    if !all_modes {
+    if !show_all {
         display
             .lines(args.lines)
             .words(args.words)
@@ -29,6 +31,7 @@ fn run(args: &cli::Args) -> std::io::Result<()> {
             .bytes(args.bytes);
     }
 
+    // Print the formatted output
     println!("{}", display.display(results));
     Ok(())
 }
