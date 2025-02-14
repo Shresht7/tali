bf17dabd1e5319eecac650fdc2ff670d6f796a9e
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-02-14 17:11:12 +0530

    Redefine the macro syntax

--- src/lib/language.rs
+++ src/lib/language.rs
@@ -1,10 +1,11 @@
 /// Describe a macro to generate the [`Language`] enum
 macro_rules! define_languages {
     ( $(
-        $language:ident from [$($extension:literal),*]              // Matches: Rust from ["rs",...]
-        $(with RGB($colorR:expr, $colorG:expr, $colorB:expr))?      // Matches: with RGB(255, 165, 0)
-        $(as $display:literal)?                                     // (Optionally) Matches: as RS
-    ),* $(,)? ) => {                                                // Matches: ,
+        $language:ident                                             // Matches: Rust                    | Language Identifier
+        $(as $display:literal)?                                     // Matches: as "RS"                 | Optional display name
+        from [$($extension:literal),*]                              // Matches: from ["rs", ...]        | file-extension list
+        $(with RGB($colorR:expr, $colorG:expr, $colorB:expr))?      // Matches: with RGB(255, 165, 0)   | RGB color for the language
+    ),* $(,)? ) => {                                                // Matches: ,                       | Optional trailing comma
         #[derive(Debug, Clone, PartialEq, Eq, Hash)]
         pub enum Language {
             $($language),*,
@@ -58,30 +59,30 @@ macro_rules! define_languages {
 
 // Holy cow, macros are witchcraft
 define_languages! {
-    Text        from ["txt", "text"]     with RGB(255, 255, 255),
-    Rust        from ["rs"]              with RGB(255, 165, 0),
-    Markdown    from ["md", "markdown"]  with RGB(0, 102, 204),
-    TOML        from ["toml"]            with RGB(120, 120, 120),
-    JSON        from ["json", "jsonc"]   with RGB(255, 224, 102),
-    JavaScript  from ["js"]              with RGB(247, 223, 30),
-    TypeScript  from ["ts"]              with RGB(0, 122, 204),
-    Go          from ["go"]              with RGB(0, 173, 216),
-    C           from ["c"]               with RGB(70, 70, 240),
-    CPP         from ["cpp"]             with RGB(45, 45, 255)       as "C++",
-    CSharp      from ["cs"]              with RGB(98, 164, 228)      as "C#",
-    Python      from ["py"]              with RGB(53, 114, 165),
-    Java        from ["java"]            with RGB(176, 114, 25),
-    HTML        from ["html", "htm"]     with RGB(227, 76, 38),
-    CSS         from ["css"]             with RGB(86, 61, 124),
-    YAML        from ["yaml", "yml"]     with RGB(255, 255, 0),
-    PHP         from ["php"]             with RGB(79, 93, 149),
-    Swift       from ["swift"]           with RGB(255, 102, 0),
-    Kotlin      from ["kt", "kts"]       with RGB(136, 58, 163),
-    Ruby        from ["rb"]              with RGB(204, 52, 51),
-    Perl        from ["pl", "pm"]        with RGB(129, 133, 149),
-    Lua         from ["lua"]             with RGB(0, 0, 255),
-    Bash        from ["sh"]              with RGB(88, 156, 88),
-    Makefile    from ["mk", "makefile"]  with RGB(48, 77, 48),
+    Text                from ["txt", "text"]     with RGB(255, 255, 255),
+    Rust                from ["rs"]              with RGB(255, 165, 0),
+    Markdown            from ["md", "markdown"]  with RGB(0, 102, 204),
+    TOML                from ["toml"]            with RGB(120, 120, 120),
+    JSON                from ["json", "jsonc"]   with RGB(255, 224, 102),
+    JavaScript          from ["js"]              with RGB(247, 223, 30),
+    TypeScript          from ["ts"]              with RGB(0, 122, 204),
+    Go                  from ["go"]              with RGB(0, 173, 216),
+    C                   from ["c"]               with RGB(70, 70, 240),
+    CPP as "C++"        from ["cpp"]             with RGB(45, 45, 255),
+    CSharp as "C#"      from ["cs"]              with RGB(98, 164, 228),
+    Python              from ["py"]              with RGB(53, 114, 165),
+    Java                from ["java"]            with RGB(176, 114, 25),
+    HTML                from ["html", "htm"]     with RGB(227, 76, 38),
+    CSS                 from ["css"]             with RGB(86, 61, 124),
+    YAML                from ["yaml", "yml"]     with RGB(255, 255, 0),
+    PHP                 from ["php"]             with RGB(79, 93, 149),
+    Swift               from ["swift"]           with RGB(255, 102, 0),
+    Kotlin              from ["kt", "kts"]       with RGB(136, 58, 163),
+    Ruby                from ["rb"]              with RGB(204, 52, 51),
+    Perl                from ["pl", "pm"]        with RGB(129, 133, 149),
+    Lua                 from ["lua"]             with RGB(0, 0, 255),
+    Bash                from ["sh"]              with RGB(88, 156, 88),
+    Makefile            from ["mk", "makefile"]  with RGB(48, 77, 48),
 }
 
 #[cfg(test)]
