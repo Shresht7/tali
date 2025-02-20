4921d0df50cabefc219eddd39ccdb65f51cd9bee
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-02-20 13:32:48 +0530

    Support Additional Output Formats: CSV, TSV Fixes #4

---
+++ src/lib/output/delimiter.rs
@@ -0,0 +1,112 @@
+use crate::scanner::{File, ScanResults};
+
+use super::{Config, Formatter};
+
+#[derive(Debug, Default)]
+pub struct DelimiterFormatter<'a> {
+    delimiter: &'a str,
+}
+
+impl<'a> DelimiterFormatter<'a> {
+    pub fn with(delimiter: &'a str) -> DelimiterFormatter<'a> {
+        DelimiterFormatter { delimiter }
+    }
+}
+
+impl Formatter for DelimiterFormatter<'_> {
+    fn format(&self, results: &ScanResults, config: &Config) -> String {
+        let mut res = String::new();
+
+        if config.header {
+            res.push_str(&self.build_header(config));
+        }
+
+        for file in &results.files {
+            res.push_str(&self.build_row(file, results, config));
+        }
+
+        if config.footer {
+            res.push_str(&self.build_footer(results, config));
+        }
+
+        res
+    }
+}
+
+impl DelimiterFormatter<'_> {
+    // Helper function to select columns
+    fn selected_columns<T>(&self, values: T, config: &Config) -> Vec<T::Item>
+    where
+        T: IntoIterator,
+        T::IntoIter: ExactSizeIterator,
+        T::Item: Clone,
+    {
+        let options = [
+            config.language,
+            config.path,
+            config.lines,
+            config.words,
+            config.chars,
+            config.bytes,
+        ];
+        values
+            .into_iter()
+            .enumerate()
+            .filter_map(|(i, v)| options[i].then_some(v.clone()))
+            .collect()
+    }
+
+    fn build_header(&self, config: &Config) -> String {
+        self.selected_columns(
+            ["Language", "Path", "Lines", "Words", "Chars", "Bytes"].map(String::from),
+            config,
+        )
+        .join(self.delimiter)
+            + "\n"
+    }
+
+    fn build_row(&self, file: &File, _results: &ScanResults, config: &Config) -> String {
+        let mut cols = Vec::new();
+
+        if config.language {
+            cols.push(file.language.to_string());
+        }
+
+        if config.path {
+            cols.push(file.path.to_string_lossy().to_string());
+        }
+
+        if config.lines {
+            cols.push(file.lines.to_string());
+        }
+
+        if config.words {
+            cols.push(file.words.to_string());
+        }
+
+        if config.chars {
+            cols.push(file.chars.to_string());
+        }
+
+        if config.bytes {
+            cols.push(file.bytes.to_string());
+        }
+
+        cols.join(self.delimiter) + "\n"
+    }
+
+    fn build_footer(&self, results: &ScanResults, config: &Config) -> String {
+        self.selected_columns(
+            [
+                "Total".to_string(),
+                "".to_string(),
+                results.total.lines.to_string(),
+                results.total.words.to_string(),
+                results.total.chars.to_string(),
+                results.total.bytes.to_string(),
+            ],
+            config,
+        )
+        .join(self.delimiter)
+    }
+}
