3188497f8f29f0a52130a9266c622a8608708bb6
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-03-13 01:19:32 +0530

    Add more languages
    - Astro, Svelte, React
    - CSV, TSV
    - PowerShell
    - SVG, XML
    
    #7

--- src/lib/helpers/language.rs
+++ src/lib/helpers/language.rs
@@ -61,15 +61,17 @@ macro_rules! define_languages {
 
 // Holy cow, macros are witchcraft
 define_languages! {
+    Astro               from ["astro"]                  with RGB(255, 69, 0),
     Bash                from ["sh"]                     with RGB(88, 156, 88),
     C                   from ["c"]                      with RGB(70, 70, 240),
     CPP as "C++"        from ["cpp"]                    with RGB(45, 45, 255),
     CSharp as "C#"      from ["cs"]                     with RGB(98, 164, 228),
     CSS                 from ["css"]                    with RGB(86, 61, 124),
+    CSV                 from ["csv"]                    with RGB(0, 123, 255),
     Go                  from ["go"]                     with RGB(0, 173, 216),
     HTML                from ["html", "htm"]            with RGB(227, 76, 38),
     Java                from ["java"]                   with RGB(176, 114, 25),
-    JavaScript          from ["js", ".mjs", ".cjs"]     with RGB(247, 223, 30),
+    JavaScript          from ["js", "mjs", "cjs"]       with RGB(247, 223, 30),
     JSON                from ["json", "jsonc"]          with RGB(255, 224, 102),
     Kotlin              from ["kt", "kts"]              with RGB(136, 58, 163),
     Lua                 from ["lua"]                    with RGB(0, 0, 255),
@@ -77,13 +79,19 @@ define_languages! {
     Markdown            from ["md", "markdown"]         with RGB(0, 102, 204),
     Perl                from ["pl", "pm"]               with RGB(129, 133, 149),
     PHP                 from ["php"]                    with RGB(79, 93, 149),
+    PowerShell          from ["ps1", "psm1", "psd1"]    with RGB(1, 36, 86),
     Python              from ["py"]                     with RGB(53, 114, 165),
+    React               from ["jsx", "tsx"]             with RGB(0, 122, 204),
     Ruby                from ["rb"]                     with RGB(204, 52, 51),
     Rust                from ["rs"]                     with RGB(255, 165, 0),
+    Svelte              from ["svelte"]                 with RGB(255, 62, 0),
+    SVG                 from ["svg"]                    with RGB(255, 181, 0),
     Swift               from ["swift"]                  with RGB(255, 102, 0),
     Text                from ["txt", "text"]            with RGB(255, 255, 255),
     TOML                from ["toml"]                   with RGB(120, 120, 120),
+    TSV                 from ["tsv"]                    with RGB(0, 123, 255),
     TypeScript          from ["ts"]                     with RGB(0, 122, 204),
+    XML                 from ["xml"]                    with RGB(255, 153, 51),
     YAML                from ["yaml", "yml"]            with RGB(255, 255, 0),
 }
 
