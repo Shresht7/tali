2905291fa0e14a69f090e686fb34d02f8d824fba
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-03-13 21:08:16 +0530

    Move `ScanResults` into its own file

---
+++ src/lib/scanner/results.rs
@@ -0,0 +1,88 @@
+use serde::Serialize;
+
+use std::collections::HashMap;
+
+use super::{File, Max, Totals};
+
+// ------------
+// SCAN RESULTS
+// ------------
+
+/// Represents the aggregate scan results
+#[derive(Debug, Serialize)]
+pub struct ScanResults {
+    /// The collection of all file results containing information like the number of lines, words, chars and bytes
+    pub files: Vec<File>,
+    /// The aggregate total number of lines, words, chars and bytes
+    pub total: Totals,
+    /// The max values for the number of lines, words, chars, and bytes across the results
+    pub max: Max,
+}
+
+impl ScanResults {
+    /// Groups the files by language and returns a new [`ScanResults`] instance
+    pub fn group_by_language(&self) -> ScanResults {
+        // Group the files by language in a HashMap
+        let mut groups = HashMap::new();
+        for file in self.files.iter().cloned() {
+            groups
+                .entry(file.language.clone())
+                .or_insert_with(Vec::new)
+                .push(file)
+        }
+
+        // Create a new condensed ScanResult
+        let mut files = Vec::new();
+        let mut total = Totals::default();
+        let mut max = Max::default();
+        for lang in groups.keys() {
+            if let Some(v) = groups.get(lang) {
+                if let Some(file) = v.iter().cloned().reduce(|acc, e| acc + e) {
+                    total.add(&file);
+                    max.track(&file);
+                    files.push(file)
+                }
+            }
+        }
+        ScanResults { files, total, max }
+    }
+
+    /// Sort the [`ScanResults`] files based on the given column and sort order
+    pub fn sort_by(&mut self, category: &str, order: &SortOrder) {
+        match category.to_lowercase().as_str() {
+            "line" | "lines" => self.files.sort_by(|a, b| match order {
+                SortOrder::Ascending => a.lines.cmp(&b.lines),
+                SortOrder::Descending => b.lines.cmp(&a.lines),
+            }),
+            "word" | "words" => self.files.sort_by(|a, b| match order {
+                SortOrder::Ascending => a.words.cmp(&b.words),
+                SortOrder::Descending => b.words.cmp(&a.words),
+            }),
+            "char" | "chars" => self.files.sort_by(|a, b| match order {
+                SortOrder::Ascending => a.chars.cmp(&b.chars),
+                SortOrder::Descending => b.chars.cmp(&a.chars),
+            }),
+            "byte" | "bytes" | _ => self.files.sort_by(|a, b| match order {
+                SortOrder::Ascending => a.bytes.cmp(&b.bytes),
+                SortOrder::Descending => b.bytes.cmp(&a.bytes),
+            }),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum SortOrder {
+    Ascending,
+    Descending,
+}
+
+impl std::str::FromStr for SortOrder {
+    type Err = String;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_lowercase().as_str() {
+            "a" | "asc" | "ascending" => Ok(Self::Ascending),
+            "d" | "desc" | "descending" => Ok(Self::Descending),
+            x => Err(format!("Invalid sorting order: {x}")),
+        }
+    }
+}
