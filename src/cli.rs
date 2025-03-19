cc3d1ff374afbf30c8cd4a76ebe0270a34ec84c2
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-03-20 02:24:39 +0530

    Refactor how sort_by` default is determined

--- src/cli.rs
+++ src/cli.rs
@@ -43,8 +43,8 @@ pub struct Args {
 
     // ** === SORTING OPTIONS === **
     /// Sort on category
-    #[clap(long, default_value = "bytes")]
-    pub sort_by: String,
+    #[clap(long)]
+    pub sort_by: Option<String>,
 
     /// The order in which to sort
     #[clap(long, default_value = "descending")]
@@ -143,16 +143,18 @@ impl Args {
             self.chars = true;
             self.bytes = true;
             self.graph = true;
-        } else {
-            // If bytes is not visible, update the default sort
+        }
+
+        // Determine the default sort
+        if self.sort_by.is_none() {
             if self.lines {
-                self.sort_by = "lines".into()
+                self.sort_by = Some("lines".into())
             } else if self.words {
-                self.sort_by = "words".into()
+                self.sort_by = Some("words".into())
             } else if self.chars {
-                self.sort_by = "chars".into()
+                self.sort_by = Some("chars".into())
             } else {
-                self.sort_by = "bytes".into()
+                self.sort_by = Some("bytes".into())
             }
         }
 
@@ -162,6 +164,8 @@ impl Args {
 
 impl From<&Args> for Config {
     fn from(args: &Args) -> Self {
+        let sort_by = args.sort_by.clone().unwrap_or("bytes".into());
+        let graph_by = args.graph_by.clone().unwrap_or(sort_by.clone());
         Self {
             path: !args.group,
 
@@ -173,12 +177,12 @@ impl From<&Args> for Config {
             group_by_language: args.group,
 
             graph: args.graph,
-            graph_by: args.graph_by.clone().unwrap_or(args.sort_by.clone()),
+            graph_by,
             graph_fill: args.graph_fill.clone(),
             graph_blank: args.graph_blank.clone(),
             graph_size: args.graph_size,
 
-            sort_by: args.sort_by.clone(),
+            sort_by,
             sort_order: args.sort_order,
 
             header: !args.no_header,
